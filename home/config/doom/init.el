d86cdaefe84f928855a71227baf7591686d6743c
alternateved <alternateved@gmail.com>
2021-07-17 15:28:20 +0200

    Remove Haskell lsp

--- home/config/doom/init.el
+++ home/config/doom/init.el
@@ -130,7 +130,7 @@
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;gdscript          ; the language you waited for
        ;;(go +lsp)         ; the hipster dialect
-       (haskell +dante)    ; a language that's lazier than I am
+       haskell             ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
        (json +lsp)              ; At least it ain't XML
