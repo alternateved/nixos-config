7e3558de081c96927021a12b2adfbb05bed4e620
alternateved <alternateved@gmail.com>
2021-07-20 21:54:20 +0200

    Add some tweaks

--- home/config/doom/config.el
+++ home/config/doom/config.el
@@ -2,33 +2,58 @@
 
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
-;;
-;; scroll one line at a time (less "jumpy" than defaults)
+(setq undo-limit 80000000                         ; Raise undo-limit to 80Mb
+      evil-want-fine-undo t                       ; By default while in insert all changes are one big blob. Be more granular
+      auto-save-default t                         ; Nobody likes to loose work, I certainly don't
+      truncate-string-ellipsis "…"                ; Unicode ellispis are nicer than "...", and also save /precious/ space
+      password-cache-expiry nil                   ; I can trust my computers ... can't I?
+      scroll-margin 2)
 
-;; scroll one line at a time (less "jumpy" than defaults)
-(setq doom-font (font-spec :family "JetBrains Mono Nerd Font" :size 15 :weight 'medium)
-       doom-variable-pitch-font (font-spec :family "JetBrains Mono Nerd Font" :size 16))
+(global-subword-mode 1)                           ; Iterate through CamelCase words
 
-;; (setq doom-font (font-spec :family "FiraCode Nerd Font Mono" :size 13 :weight 'medium)
-;;        doom-variable-pitch-font (font-spec :family "FiraCode Nerd Font Mono" :size 14))
+;; Lots of autocompletion
+(after! company
+  (setq company-idle-delay 0.5
+        company-minimum-prefix-length 2)
+  (setq company-show-quick-access t)
+  (add-hook 'evil-normal-state-entry-hook #'company-abort)) ; make aborting less annoying.
+
+(setq-default history-length 1000)
+(setq-default prescient-history-length 1000)
+
+;; Some sane settings in evil mode
+(after! evil
+  (setq evil-ex-substitute-global t     ; I like my s/../.. to by global by default
+        evil-move-cursor-back nil       ; Don't move the block cursor when toggling insert mode
+        evil-kill-on-visual-paste nil)) ; Don't put overwritten text in the kill ring
+
+;; Which-key improvements
+(setq which-key-idle-delay 0.5)                   ; I need the help, I really do
+
+(setq which-key-allow-multiple-replacements t)
+(after! which-key
+  (pushnew!
+   which-key-replacement-alist
+   '(("" . "\\`+?evil[-:]?\\(?:a-\\)?\\(.*\\)") . (nil . "◂\\1"))
+   '(("\\`g s" . "\\`evilem--?motion-\\(.*\\)") . (nil . "◃\\1"))
+   ))
+
+;; Zen improvements
+(setq +zen-text-scale 0.8)
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name "Tomasz Hołubowicz"
       user-mail-address "alternateved@gmail.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
+;; Font settings
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(setq doom-font (font-spec :family "JetBrains Mono Nerd Font" :size 15 :weight 'medium)
+      doom-big-font (font-spec :family "JetBrains Mono Nerd Font" :size 20)
+      doom-variable-pitch-font (font-spec :family "Overpass" :size 15))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
