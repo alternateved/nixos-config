9bccbc5eb88e7067b0d6c74691a747c05513c2cc
alternateved <alternateved@gmail.com>
2021-08-03 12:56:07 +0200

    Add colors module

---
+++ home/colors.nix
@@ -0,0 +1,24 @@
+{
+  background = "#18181B";
+  foreground = "#E4E4E8";
+
+  black = "#18181B";
+  gray = "#4B5254";
+  red = "#CD5C60";
+  green = "#6FB593";
+  yellow = "#EED891";
+  blue = "#91B9C7";
+  magenta = "#845A84";
+  cyan = "#4D9391";
+  white = "#E4E4E8";
+
+  bright-black = "#161618";
+  bright-gray = "#879193";
+  bright-red = "#C93237";
+  bright-green = "#35BF88";
+  bright-yellow = "#DBAC66";
+  bright-blue = "#3B84CC";
+  bright-magenta = "#D24B83";
+  bright-cyan = "#68F3CA";
+  bright-white = "#EFEFF1";
+}
