39dd721fe809a05709c5aa2ab305b3948496305a
alternateved <alternateved@gmail.com>
2021-08-04 22:13:33 +0200

    Modularize theme related declarations

---
+++ home/theme.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }: {
+  dconf.enable = false;
+  xdg = {
+    enable = true;
+    userDirs.enable = true;
+  };
+  gtk = {
+    enable = true;
+    theme = {
+      package = pkgs.materia-theme;
+      name = "Materia-dark-compact";
+    };
+    gtk3.extraConfig = { gtk-application-prefer-dark-theme = 1; };
+    iconTheme = {
+      package = pkgs.papirus-icon-theme;
+      name = "Papirus-Dark";
+    };
+    font = {
+      package = pkgs.fira;
+      name = "Fira Sans Regular 12";
+    };
+  };
+  qt = {
+    enable = true;
+    platformTheme = "gtk";
+  };
+  xsession.pointerCursor = {
+    package = pkgs.vanilla-dmz;
+    name = "Vanilla-DMZ";
+    size = 16;
+  };
+}
