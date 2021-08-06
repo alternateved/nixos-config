e12f9a4539b4a936d348a942ccb76b0d357480c5
alternateved <alternateved@gmail.com>
2021-08-06 14:59:33 +0200

    Minor changes

--- home/programs/alacritty.nix
+++ home/programs/alacritty.nix
@@ -6,7 +6,7 @@ in {
       background_opacity = 1.0;
       window = { decorations = "none"; };
       font = {
-        size = 8.0;
+        size = 9.0;
         normal.family = "Iosevka Nerd Font Mono";
         bold.family = "Iosevka Nerd Font Mono";
         italic.family = "Iosevka Nerd Font Mono";
