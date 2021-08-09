8b9a980eb79e057f6c9ded080a97e0e2b93c4185
alternateved <alternateved@gmail.com>
2021-08-09 12:21:00 +0200

    Enable emacs service

--- home/modules/emacs.nix
+++ home/modules/emacs.nix
@@ -1,17 +1,21 @@
-{ config, lib, pkgs, ... }:
-
-{
-
+{ config, lib, pkgs, ... }: {
   programs = {
     emacs = {
       enable = true;
       package = pkgs.emacsGcc;
+      extraPackages = (epkgs: [ epkgs.vterm ]);
     };
-
   };
 
-  services = {
-    emacs.package = pkgs.emacsUnstable;
+  services = { emacs.enable = true; };
 
+  home = {
+    sessionVariables = { DOOMDIR = "home/alternateved/.config/doom"; };
+
+    file.".doom.d" = {
+      source = ../config/doom;
+      recursive = true;
+      onChange = "doom -y sync -u";
+    };
   };
 }
