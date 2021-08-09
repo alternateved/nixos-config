09d362163a4fba6bf1b4476f7637fe363a4e1ad3
alternateved <alternateved@gmail.com>
2021-08-09 22:43:58 +0200

    Add event org-capture template

--- home/config/doom/config.el
+++ home/config/doom/config.el
@@ -112,7 +112,9 @@
 
 (after! org-capture
   (setq org-capture-templates
-        '(("t" "Personal task" entry (file+headline "~/Documents/org/todo.org" "Tasks")
+        '(("n" "Personal note" entry (file+headline "~/Documents/org/notes.org" "Notes")
+           "* %? \n%U" :empty-lines 1)
+          ("t" "Personal task" entry (file+headline "~/Documents/org/todo.org" "Tasks")
            "* TODO %? \n%U" :empty-lines 1)
           ("s" "Scheduled task" entry (file+headline "~/Documents/org/todo.org" "Tasks")
            "* TODO %? \nSCHEDULED: %^t\n%U" :empty-lines 1)
@@ -120,8 +122,8 @@
            "* TODO %? \n  DEADLINE: %^t" :empty-lines 1)
           ("m" "Meeting" entry (file+headline "~/Documents/org/agenda.org" "Agenda")
            "* MEETING: %? \nSCHEDULED: %^t\n%U" :empty-lines 1)
-          ("n" "Personal note" entry (file+headline "~/Documents/org/notes.org" "Notes")
-           "* %? \n%U" :empty-lines 1))))
+          ("e" "Event" entry (file+headline "~/Documents/org/event.org" "Events")
+           "** %? \n %^T\n%U" :empty-lines 1))))
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
