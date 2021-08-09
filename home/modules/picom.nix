76348c30f23affd95cf237b0e183725824575378
alternateved <alternateved@gmail.com>
2021-08-09 12:20:52 +0200

    Fix file

--- home/modules/picom.nix
+++ home/modules/picom.nix
@@ -5,18 +5,21 @@
     backend = "glx";
     experimentalBackends = true;
 
+    ### BLUR ###
+    blur = true;
+    blurExclude = [
+      "class_g = 'Firefox Developer Edition' && argb"
+      "_GTK_FRAME_EXTENTS@:c"
+    ];
+
     ### FADE ###
     fade = false;
     fadeDelta = 10;
     fadeSteps = [ (3.0e-2) (3.0e-2) ];
     fadeExclude = [ "class_g = 'slop'" ];
-    no-fading-openclose = false;
-    no-fading-destroyed-argb = true;
 
     ### SHADOW ###
     shadow = false;
-    shadow-radius = 7;
-    shadow-color = "#000000";
     shadowOffsets = [ (-7) (-7) ];
     shadowOpacity = 0.75;
     shadowExclude = [
@@ -27,47 +30,30 @@
       "_GTK_FRAME_EXTENTS@:c"
       "_NET_WM_STATE@:32a *= '_NET_WM_STATE_HIDDEN'"
     ];
+    extraOptions = ''
+      blur: {
+        method = "dual_kawase";
+        strength = 6.9;
+      }
 
-    ### BLUR ###
-    blur = {
-      method = "kawase";
-      strength = "7.0";
-    };
-    blur-background = false;
-    blur-background-frame = false;
-    blur-background-fixed = false;
-    blurExclude = [
-      "class_g = 'Firefox Developer Edition' && argb"
-      "_GTK_FRAME_EXTENTS@:c"
-    ];
+      frame-opacity = 1.0;
+      inactive-opacity-override = false;
+      active-opacity = 1.0;
+      focus-exclude = [ "class_g ?= 'rofi'" "class_g ?= 'Steam'" ];
 
-    ### Animations ###
-    transition-length = 150;
-    transition-pow-x = 0.1;
-    transition-pow-y = 0.1;
-    transition-pow-w = 0.1;
-    transition-pow-h = 0.1;
-    size-transition = true;
+      mark-wmwin-focused = true;
+      mark-ovredir-focused = true;
+      detect-rounded-corners = true;
+      detect-client-opacity = true;
 
-    ### TRANSPARENCY / OPACITY ###
-    frame-opacity = 1.0;
-    inactive-opacity-override = false;
-    active-opacity = 1.0;
-    focus-exclude = [ "class_g ?= 'rofi'" "class_g ?= 'Steam'" ];
+      unredir-if-possible-exclude = [ ];
+      detect-transient = true;
+      detect-client-leader = true;
 
-    ### OTHER ###
-    mark-wmwin-focused = true;
-    mark-ovredir-focused = true;
-    detect-rounded-corners = true;
-    detect-client-opacity = true;
-
-    unredir-if-possible-exclude = [ ];
-    detect-transient = true;
-    detect-client-leader = true;
-
-    invert-color-include = [ ];
-    glx-no-stencil = true;
-    use-damage = false;
-    transparent-clipping = false;
+      invert-color-include = [ ];
+      glx-no-stencil = true;
+      use-damage = false;
+      transparent-clipping = false;
+    '';
   };
 }
