7d210fadfe4976539a0a4600624fe4a6f18e049a
alternateved <alternateved@gmail.com>
2021-08-09 20:05:53 +0200

    Enable nix-community binary cache

--- system/default.nix
+++ system/default.nix
@@ -144,12 +144,15 @@ in {
       dates = [ "weekly" ];
     };
     trustedUsers = [ "alternateved" "root" ];
+    binaryCaches = [ "https://nix-community.cachix.org/" ];
+    binaryCachePublicKeys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+    package = pkgs.nixUnstable;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
   };
-
-  nix.package = pkgs.nixUnstable;
-  nix.extraOptions = ''
-    experimental-features = nix-command flakes
-  '';
 
   system.stateVersion = "21.05";
 }
