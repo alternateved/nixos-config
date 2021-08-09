8ce478250a26103ae05228a4cb66825c6cad346a
alternateved <alternateved@gmail.com>
2021-08-09 12:01:04 +0200

    Minor changes

--- home/modules/picom.nix
+++ home/modules/picom.nix
@@ -2,16 +2,72 @@
   services.picom = {
     enable = true;
     vSync = true;
-    fade = false;
-    shadow = false;
-    blur = true;
-    shadowExclude = [ "class_g = 'Firefox Developer Edition' && argb" ];
-    blurExclude = [ "class_g = 'Firefox Developer Edition' && argb" ];
     backend = "glx";
     experimentalBackends = true;
-    extraOptions = ''
-      use-damage = true;
-      blur-strength = 12;
-    '';
+
+    ### FADE ###
+    fade = false;
+    fadeDelta = 10;
+    fadeSteps = [ (3.0e-2) (3.0e-2) ];
+    fadeExclude = [ "class_g = 'slop'" ];
+    no-fading-openclose = false;
+    no-fading-destroyed-argb = true;
+
+    ### SHADOW ###
+    shadow = false;
+    shadow-radius = 7;
+    shadow-color = "#000000";
+    shadowOffsets = [ (-7) (-7) ];
+    shadowOpacity = 0.75;
+    shadowExclude = [
+      "class_g = 'Firefox Developer Edition' && argb"
+      "fullscreen"
+      "! name~=''"
+      "!WM_CLASS:s"
+      "_GTK_FRAME_EXTENTS@:c"
+      "_NET_WM_STATE@:32a *= '_NET_WM_STATE_HIDDEN'"
+    ];
+
+    ### BLUR ###
+    blur = {
+      method = "kawase";
+      strength = "7.0";
+    };
+    blur-background = false;
+    blur-background-frame = false;
+    blur-background-fixed = false;
+    blurExclude = [
+      "class_g = 'Firefox Developer Edition' && argb"
+      "_GTK_FRAME_EXTENTS@:c"
+    ];
+
+    ### Animations ###
+    transition-length = 150;
+    transition-pow-x = 0.1;
+    transition-pow-y = 0.1;
+    transition-pow-w = 0.1;
+    transition-pow-h = 0.1;
+    size-transition = true;
+
+    ### TRANSPARENCY / OPACITY ###
+    frame-opacity = 1.0;
+    inactive-opacity-override = false;
+    active-opacity = 1.0;
+    focus-exclude = [ "class_g ?= 'rofi'" "class_g ?= 'Steam'" ];
+
+    ### OTHER ###
+    mark-wmwin-focused = true;
+    mark-ovredir-focused = true;
+    detect-rounded-corners = true;
+    detect-client-opacity = true;
+
+    unredir-if-possible-exclude = [ ];
+    detect-transient = true;
+    detect-client-leader = true;
+
+    invert-color-include = [ ];
+    glx-no-stencil = true;
+    use-damage = false;
+    transparent-clipping = false;
   };
 }
