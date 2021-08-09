d0958dfcb390558e95a214dd6e1d46ebbda0ce43
alternateved <alternateved@gmail.com>
2021-08-09 19:51:04 +0200

    Enable Xresources theme

--- home/modules/theme.nix
+++ home/modules/theme.nix
@@ -20,7 +20,7 @@ in {
     };
     font = {
       package = null;
-      name = "Iosevka Aile Semibold 11";
+      name = "Iosevka Aile Medium 11";
     };
   };
   qt = {
@@ -28,20 +28,55 @@ in {
     platformTheme = "gtk";
   };
   xresources.extraConfig = ''
-    Xft.antialias: 1
-    Xft.hinting: 1
-    Xft.autohint: 0
-    Xft.hintstyle: hintslight
-    Xft.rgba: rgb
-    Xft.lcdfilter: lcddefault
+     Xft.antialias: 1
+     Xft.hinting: 1
+     Xft.autohint: 0
+     Xft.hintstyle: hintslight
+     Xft.rgba: rgb
+     Xft.lcdfilter: lcddefault
 
-    Sxiv.background: ${colors.background}
-    Sxiv.foreground: ${colors.foreground}
-    Sxiv.font: Iosevka Nerd Font Mono:style=regular:size=11
+     Sxiv.background: ${colors.background}
+     Sxiv.foreground: ${colors.foreground}
+     Sxiv.font: Iosevka Nerd Font Mono:style=regular:size=11
 
-    !Xmessage.font: Iosevka Nerd Font Mono:pixelsize=12
-    *.foreground: ${colors.background}
-    *.background: ${colors.foreground}
+     Xmessage.font: Iosevka Nerd Font Mono:pixelsize=12
+
+     *.foreground: ${colors.background}
+     *.background: ${colors.foreground}
+
+    *.cursorColor: ${colors.foreground}
+
+     ! Black
+     *.color0: ${colors.black}
+     *.color8: ${colors.bright-black}
+
+     ! Red
+     *.color1: ${colors.red}
+     *.color9: ${colors.bright-red}
+
+     ! Green
+     *.color2: ${colors.green}
+     *.color10: ${colors.bright-green}
+
+     ! Yellow
+     *.color3: ${colors.yellow}
+     *.color11: ${colors.bright-yellow}
+
+     ! Blue
+     *.color4: ${colors.blue}
+     *.color12: ${colors.bright-blue}
+
+     ! Magenta
+     *.color5: ${colors.magenta}
+     *.color13: ${colors.bright-magenta}
+
+     ! Cyan
+     *.color6: ${colors.cyan}
+     *.color14: ${colors.bright-cyan}
+
+     ! White
+     *.color7: ${colors.white}
+     *.color15: ${colors.bright-white}
   '';
   xsession.pointerCursor = {
     package = pkgs.vanilla-dmz;
