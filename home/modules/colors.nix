cdac2b0e7c0e29b94c11f29fef8651f5d653c10a
alternateved <alternateved@gmail.com>
2021-08-12 18:32:27 +0200

    Switch yellow colors

--- home/modules/colors.nix
+++ home/modules/colors.nix
@@ -5,7 +5,7 @@
   black = "#4B5254";
   red = "#CD5C60";
   green = "#6FB593";
-  yellow = "#EED891";
+  yellow = "#DBAC66";
   blue = "#91B9C7";
   magenta = "#845A84";
   cyan = "#4D9391";
@@ -14,7 +14,7 @@
   bright-black = "#879193";
   bright-red = "#C93237";
   bright-green = "#35BF88";
-  bright-yellow = "#DBAC66";
+  bright-yellow = "#EED891";
   bright-blue = "#3B84CC";
   bright-magenta = "#D24B83";
   bright-cyan = "#68F3CA";
