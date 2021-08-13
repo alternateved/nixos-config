7bb8dc108722f8efd5ffacf032bef8eaebf2fb7c
alternateved <alternateved@gmail.com>
2021-08-13 14:37:40 +0200

    Fix script to spawn only one bar on one screen

--- home/config/polybar/launch.sh
+++ home/config/polybar/launch.sh
@@ -6,8 +6,16 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Check for a number of outputs
+outputs=$(polybar --list-monitors | cut -d":" -f1)
+outputs_length=$(wc -w <<< "$outputs")
+
 # Launch the bar
-for m in $(polybar --list-monitors | cut -d":" -f1); do
+if [ "$outputs_length" == 1 ]; then
     MONITOR=$m polybar -q main &
-    MONITOR=$m polybar -q aux &
-done
+else
+    for m in $outputs; do
+        MONITOR=$m polybar -q main &
+        MONITOR=$m polybar -q aux &
+    done
+fi
