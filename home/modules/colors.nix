ced68be2f4cef641acbd84413e8dfcdd2cbc0170
alternateved <alternateved@gmail.com>
2021-08-17 14:06:20 +0200

    Minor changes

--- home/modules/colors.nix
+++ home/modules/colors.nix
@@ -21,13 +21,12 @@
   bright-white = "#EFEFF1";
 }
 
-# # Solarized theme
+# # Solarized light theme
 # {
 #   background = "#fdf6e3";
 #   foreground = "#586e75";
 
 #   black = "#073642";
-#   gray = "#657b83";
 #   red = "#dc322f";
 #   green = "#859900";
 #   yellow = "#b58900";
@@ -37,7 +36,30 @@
 #   white = "#eee8d5";
 
 #   bright-black = "#002b36";
-#   bright-gray = "#93a1a1";
+#   bright-red = "#cb4b16";
+#   bright-green = "#586e75";
+#   bright-yellow = "#657b83";
+#   bright-blue = "#839496";
+#   bright-magenta = "#6c71c4";
+#   bright-cyan = "#93a1a1";
+#   bright-white = "#fdf6e3";
+# }
+
+# # Solarized dark theme
+# {
+#   background = "#002b36";
+#   foreground = "#839496";
+
+#   black = "#073642";
+#   red = "#dc322f";
+#   green = "#859900";
+#   yellow = "#b58900";
+#   blue = "#268bd2";
+#   magenta = "#d33682";
+#   cyan = "#2aa198";
+#   white = "#eee8d5";
+
+#   bright-black = "#002b36";
 #   bright-red = "#cb4b16";
 #   bright-green = "#586e75";
 #   bright-yellow = "#657b83";
