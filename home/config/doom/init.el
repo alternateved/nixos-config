83a625563fe82d2d3d24a4443deb62fdd1397978
alternateved <alternateved@gmail.com>
2021-08-18 15:48:28 +0200

    Remove emacs from everywhere

--- home/config/doom/init.el
+++ home/config/doom/init.el
@@ -184,7 +184,7 @@
        :app
        ;;calendar
        ;;emms
-       everywhere          ; *leave* Emacs!? You must be joking
+       ;;everywhere        ; *leave* Emacs!? You must be joking
        ;;irc               ; how neckbeards socialize
        ;;(rss +org)        ; emacs as an RSS reader
        ;;twitter           ; twitter client https://twitter.com/vnought
