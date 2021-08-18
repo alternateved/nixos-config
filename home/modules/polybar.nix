f4e910efd341b64a4baf91b41c0280e36158d831
alternateved <alternateved@gmail.com>
2021-08-18 15:47:35 +0200

    Add spotify module

--- home/modules/polybar.nix
+++ home/modules/polybar.nix
@@ -6,11 +6,25 @@
       githubSupport = true;
     };
     config = ../config/polybar/config.ini;
+    extraConfig = ''
+      [module/spotify]
+      type = custom/script
+      exec = ${pkgs.playerctl}/bin/playerctl --player=spotify metadata --format "{{ artist }} - {{ title }}"
+      format-prefix = "  "
+      click-left = ${pkgs.playerctl}/bin/playerctl --player=spotify play-pause
+    '';
     script = ''
-      for m in $(polybar --list-monitors | cut -d":" -f1); do
-          MONITOR=$m polybar -q main &
-          MONITOR=$m polybar -q aux &
-      done
+      outputs=$(polybar --list-monitors | cut -d":" -f1)
+      outputs_length=$(wc -w <<< "$outputs")
+
+      if [ "$outputs_length" == 1 ]; then
+        MONITOR=$m polybar -q main &
+      else
+        for m in $outputs; do
+            MONITOR=$m polybar -q main &
+            MONITOR=$m polybar -q aux &
+        done
+      fi
     '';
   };
 }
