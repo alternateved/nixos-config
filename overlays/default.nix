f4b9d6e1c134c1491adfddf3c6a346f3868cd7ef
alternateved <alternateved@gmail.com>
2021-08-20 16:43:00 +0200

    Minor changes

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,7 +3,7 @@ _: pkgs: rec {
     overrides = pkgs.lib.composeExtensions (old.overrides or (_: _: { }))
       (self: super: rec {
         alternateved-xmonad = self.callCabal2nix "alternateved-xmonad"
-          (pkgs.lib.sourceByRegex ../home/config/xmonad [
+          (pkgs.lib.sourceByRegex ../config/xmonad [
             "xmonad.hs"
             "alternateved-xmonad.cabal"
           ]) { };
