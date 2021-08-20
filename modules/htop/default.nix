f7700acd3a1cfa948ff6e78ac3da6a6129c233cc
alternateved <alternateved@gmail.com>
2021-08-21 01:08:29 +0200

    Minor changes

--- modules/htop/default.nix
+++ modules/htop/default.nix
@@ -9,11 +9,6 @@
       sort_key = 2;
       tree_view = true;
       vim_mode = true;
-
-      left_meters = [ "AllCPUs" ];
-      left_meter_modes = [ 1 ];
-      right_meters = [ "Memory" "Swap" "Tasks" "LoadAverage" "Uptime" ];
-      left_emter_modes = [ 2 2 2 2 2 ];
     };
   };
 }
