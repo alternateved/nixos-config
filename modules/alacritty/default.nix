2eb8c09250276254f911233dc3983f67dfebf39d
alternateved <alternateved@gmail.com>
2021-08-22 22:28:00 +0200

    Change font size

--- modules/alacritty/default.nix
+++ modules/alacritty/default.nix
@@ -12,7 +12,7 @@ in {
         decorations = "none";
       };
       font = {
-        size = 11.0;
+        size = 10.0;
         normal.family = "Iosevka Nerd Font";
         bold.family = "Iosevka Nerd Font";
         italic.family = "Iosevka Nerd Font";
