5f542214b49d49191eb0dcdfa799a38c033bee8b
alternateved <alternateved@gmail.com>
2021-09-03 17:26:37 +0200

    Add modus themes

--- modules/theme/colors.nix
+++ modules/theme/colors.nix
@@ -1,28 +1,4 @@
-# Kaolin dark theme
-# {
-#   background = "#18181B";
-#   foreground = "#E4E4E8";
-
-#   black = "#4B5254";
-#   red = "#CD5C60";
-#   green = "#6FB593";
-#   yellow = "#DBAC66";
-#   blue = "#91B9C7";
-#   magenta = "#845A84";
-#   cyan = "#4D9391";
-#   white = "#E4E4E8";
-
-#   bright-black = "#879193";
-#   bright-red = "#c86d6d";
-#   bright-green = "#90aea1";
-#   bright-yellow = "#EED891";
-#   bright-blue = "#C6D5E8";
-#   bright-magenta = "#ab98b5";
-#   bright-cyan = "#65a0a1";
-#   bright-white = "#EFEFF1";
-# }
-
-# Gruvbox light theme
+# Gruvbox light
 # {
 #   # hard contrast: background = "#f9f5d7"
 #   background = "#fbf1c7";
@@ -48,33 +24,33 @@
 #   bright-white = "#3c3836";
 # }
 
-# Gruvbox dark theme
-{
-  # hard contrast background - '#1d2021'
-  background = "#282828";
-  # soft contrast background - '#32302f'
-  foreground = "#ebdbb2";
+# Gruvbox dark
+# {
+#   # hard contrast background - '#1d2021'
+#   background = "#282828";
+#   # soft contrast background - '#32302f'
+#   foreground = "#ebdbb2";
 
-  black = "#504945";
-  red = "#cc241d";
-  green = "#98971a";
-  yellow = "#d79921";
-  blue = "#458588";
-  magenta = "#b16286";
-  cyan = "#689d6a";
-  white = "#a89984";
+#   black = "#504945";
+#   red = "#cc241d";
+#   green = "#98971a";
+#   yellow = "#d79921";
+#   blue = "#458588";
+#   magenta = "#b16286";
+#   cyan = "#689d6a";
+#   white = "#a89984";
 
-  bright-black = "#928374";
-  bright-red = "#fb4934";
-  bright-green = "#b8bb26";
-  bright-yellow = "#fabd2f";
-  bright-blue = "#83a598";
-  bright-magenta = "#d3869b";
-  bright-cyan = "#8ec07c";
-  bright-white = "#ebdbb2";
-}
+#   bright-black = "#928374";
+#   bright-red = "#fb4934";
+#   bright-green = "#b8bb26";
+#   bright-yellow = "#fabd2f";
+#   bright-blue = "#83a598";
+#   bright-magenta = "#d3869b";
+#   bright-cyan = "#8ec07c";
+#   bright-white = "#ebdbb2";
+# }
 
-# Solarized light theme
+# Solarized light
 # {
 #   background = "#fdf6e3";
 #   foreground = "#586e75";
@@ -99,7 +75,7 @@
 #   bright-white = "#fdf6e3";
 # }
 
-# Solarized dark theme
+# Solarized dark
 # {
 #   background = "#002b36";
 #   foreground = "#839496";
@@ -122,26 +98,74 @@
 #   bright-cyan = "#93a1a1";
 #   bright-white = "#fdf6e3";
 # }
-# Monokai Dark
-#{
-#  background = "#272822";
-#  foreground = "#F8F8F2";
-#
-#   black = "#403E41";
-#   red = "#F92672";
-#   green = "#A6E22E";
-#   yellow = "#F4BF75";
-#   blue = "#66D9EF";
-#   magenta = "#AE81FF";
-#   cyan = "#A1EFE4";
-#   white = "#F8F8F2";
-#
-#   bright-black = "#75715E";
-#   bright-red = "#F92672";
-#   bright-green = "#A6E22E";
-#   bright-yellow = "#F4BF75";
-#   bright-blue = "#66D9EF";
-#   bright-magenta = "#AE81FF";
-#   bright-cyan = "#A1EFE4";
-#   bright-white = "#F9F8F5";
-#}
+
+# Modus operandi
+{
+  background = "#ffffff";
+  foreground = "#000000";
+
+  black = "#000000";
+  red = "#a60000";
+  green = "#005e00";
+  yellow = "#813e00";
+  blue = "#0031a9";
+  magenta = "#721045";
+  cyan = "#00538b";
+  white = "#bfbfbf";
+
+  bright-black = "#595959";
+  bright-red = "#972500";
+  bright-green = "#315b00";
+  bright-yellow = "#70480f";
+  bright-blue = "#2544bb";
+  bright-magenta = "#5317ac";
+  bright-cyan = "#005a5f";
+  bright-white = "#ffffff";
+}
+
+# Modus vivendi
+# {
+#   background = "#000000";
+#   foreground = "#ffffff";
+
+#   black = "#000000";
+#   red = "#ff8059";
+#   green = "#44bc44";
+#   yellow = "#d0bc00";
+#   blue = "#2fafff";
+#   magenta = "#feacd0";
+#   cyan = "#00d3d0";
+#   white = "#bfbfbf";
+
+#   bright-black = "#595959";
+#   bright-red = "#ef8b50";
+#   bright-green = "#70b900";
+#   bright-yellow = "#c0c530";
+#   bright-blue = "#79a8ff";
+#   bright-magenta = "#b6a0ff";
+#   bright-cyan = "#6ae4b9";
+#   bright-white = "#ffffff";
+# }
+# Kaolin dark
+# {
+#   background = "#18181B";
+#   foreground = "#E4E4E8";
+
+#   black = "#4B5254";
+#   red = "#CD5C60";
+#   green = "#6FB593";
+#   yellow = "#DBAC66";
+#   blue = "#91B9C7";
+#   magenta = "#845A84";
+#   cyan = "#4D9391";
+#   white = "#E4E4E8";
+
+#   bright-black = "#879193";
+#   bright-red = "#c86d6d";
+#   bright-green = "#90aea1";
+#   bright-yellow = "#EED891";
+#   bright-blue = "#C6D5E8";
+#   bright-magenta = "#ab98b5";
+#   bright-cyan = "#65a0a1";
+#   bright-white = "#EFEFF1";
+# }
