8430a9e15eb245e562f2424fec6dd32641f7b353
alternateved <alternateved@gmail.com>
2021-09-14 22:21:52 +0200

    Remove player module

--- modules/system/polybar/default.nix
+++ modules/system/polybar/default.nix
@@ -6,13 +6,6 @@
       githubSupport = true;
     };
     config = ../../../config/polybar/config.ini;
-    extraConfig = ''
-      [module/spotify]
-      type = custom/script
-      exec = ${pkgs.playerctl}/bin/playerctl --player=spotify,ncspot metadata --format "{{ artist }} - {{ title }}"
-      format-prefix = "  "
-      click-left = ${pkgs.playerctl}/bin/playerctl --player=spotify,ncspot play-pause
-    '';
     script = ''
       outputs=$(polybar --list-monitors | cut -d":" -f1)
       outputs_length=$(wc -w <<< "$outputs")
