f915ff613f4f41137024b2413d3326827481873a
alternateved <alternateved@gmail.com>
2021-09-23 00:11:00 +0200

    Manage emacs packages with nix

--- modules/home/emacs/default.nix
+++ modules/home/emacs/default.nix
@@ -3,17 +3,95 @@
     emacs = {
       enable = true;
       package = pkgs.emacsGcc;
-      extraPackages = (epkgs: [ epkgs.vterm ]);
+      extraPackages = (epkgs:
+        with epkgs; [
+          # Package managements
+          use-package
+          auto-compile
+
+          # Cache
+          gcmh
+          no-littering
+
+          # Editor
+          smartparens
+          rainbow-delimiters
+          undo-fu
+          undo-fu-session
+
+          # Apperance
+          mood-line
+          hide-mode-line
+          all-the-icons
+          all-the-icons-completion
+
+          # Evil
+          evil
+          evil-collection
+          evil-org
+          evil-surround
+          evil-nerd-commenter
+          better-jumper
+
+          # Keybindings
+          general
+
+          # Completion framework
+          vertico
+          orderless
+          marginalia
+          consult
+
+          # Org
+          org-bullets
+          org-journal
+          hl-todo
+
+          # Tools
+          ## Mail
+          org-msg
+          ## Projectile
+          projectile
+          ## Magit
+          magit
+          ## Terminals
+          ### Eshell
+          eshell-syntax-highlighting
+          eshell-toggle
+          ### Vterm
+          vterm
+          ### Helpful packages
+          which-key
+          helpful
+          rainbow-mode
+
+          # Development
+          flycheck
+          format-all
+          ## LSP
+          lsp-mode
+          lsp-ui
+          ## Company
+          company
+          company-box
+          ## Languages
+          nix-mode
+          js2-mode
+          js2-refactor
+          web-mode
+          typescript-mode
+          haskell-mode
+          lua-mode
+          markdown-mode
+        ]);
     };
   };
   services = { emacs.enable = true; };
   home.packages = with pkgs; [
-    ## Doom dependencies
+    ## Dependencies
     git
     coreutils
     binutils
-
-    ## Optional dependencies
     fd
     ripgrep
     zstd
