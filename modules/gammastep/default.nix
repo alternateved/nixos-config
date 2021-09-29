b25620c8d0326b145681c0e2653d6af02c456808
alternateved <alternateved@gmail.com>
2021-09-29 20:49:23 +0200

    Add gammastep module

---
+++ modules/gammastep/default.nix
@@ -0,0 +1,12 @@
+{
+  home-manager.users.alternateved.services.gammastep = {
+    enable = true;
+    provider = "manual";
+    latitude = 54.7;
+    longitude = 19.4;
+    temperature = {
+      day = 5700;
+      night = 2700;
+    };
+  };
+}
