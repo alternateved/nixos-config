02406bcd607d5e4f1ce879f1afbb2e3a5f001620
alternateved <alternateved@gmail.com>
2021-10-01 00:36:09 +0200

    Define session variables with home-manager

--- modules/window-managers/sway.nix
+++ modules/window-managers/sway.nix
@@ -25,18 +25,6 @@ in {
   services.xserver.displayManager.defaultSession = "sway";
   programs.sway = {
     enable = true;
-    extraSessionCommands = ''
-      export XDG_SESSION_TYPE=wayland
-      export SDL_VIDEODRIVER=wayland
-      export GDK_BACKEND=wayland
-      export QT_QPA_PLATFORM=wayland
-      export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
-      export _JAVA_AWT_WM_NONPARENTING=1
-      export MOZ_ENABLE_WAYLAND=1
-      export MOZ_USE_XINPUT2=1
-      export XDG_CURRENT_DESKTOP=sway
-      export BMENU_BACKEND=wayland
-    '';
     extraPackages = with pkgs; [
       swayidle
       swaylock
@@ -479,6 +467,19 @@ in {
         };
       };
     };
+  };
+  home-manager.users.alternateved.home.sessionVariables = {
+    XDG_SESSION_TYPE = "wayland";
+    XDG_CURRENT_DESKTOP = "sway";
+    SDL_VIDEODRIVER = "wayland";
+    GDK_BACKEND = "wayland";
+    QT_QPA_PLATFORM = "wayland";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = 1;
+    _JAVA_AWT_WM_NONPARENTING = 1;
+    MOZ_ENABLE_WAYLAND = 1;
+    MOZ_DBUS_REMOTE = 1;
+    MOZ_USE_XINPUT2 = 1;
+    BMENU_BACKEND = "wayland";
   };
 }
 
