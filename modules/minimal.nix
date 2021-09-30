262563f705b743dcad7203d018cbff5ba5efc6f9
alternateved <alternateved@gmail.com>
2021-09-30 13:01:47 +0200

    Format code

--- modules/minimal.nix
+++ modules/minimal.nix
@@ -1,11 +1,10 @@
-{ config, lib, pkgs, inputs, ... }:
-
-{
+{ config, lib, pkgs, inputs, ... }: {
   programs.home-manager.enable = true;
   home.username = "alternateved";
   home.homeDirectory = "/home/alternateved";
 
-  imports = [ ./direnv ./fzf ./git ./htop ./neovim ./nnn ./starship/pi.nix ./zsh ];
+  imports =
+    [ ./direnv ./fzf ./git ./htop ./neovim ./nnn ./starship/pi.nix ./zsh ];
 
   home.packages = with pkgs; [ xarchiver unzip ];
 
