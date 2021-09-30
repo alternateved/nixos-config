0d61131d79ded352e1153c30d2052e7c89b98267
alternateved <alternateved@gmail.com>
2021-10-01 00:35:22 +0200

    Fix wrong GTK+ themes for native Wayland applications

--- modules/window-managers/sway.nix
+++ modules/window-managers/sway.nix
@@ -59,7 +59,22 @@ in {
     gtkUsePortal = true;
   };
 
-  home-manager.users.alternateved.wayland.windowManager.sway = {
+  home-manager.users.alternateved.wayland.windowManager.sway = let
+    gsettings = "${pkgs.glib}/bin/gsettings";
+    gnomeSchema = "org.gnome.desktop.interface";
+    importGsettings = pkgs.writeShellScript "import_gsettings.sh" ''
+      config="/home/alternateved/.config/gtk-3.0/settings.ini"
+      if [ ! -f "$config" ]; then exit 1; fi
+      gtk_theme="$(grep 'gtk-theme-name' "$config" | sed 's/.*\s*=\s*//')"
+      icon_theme="$(grep 'gtk-icon-theme-name' "$config" | sed 's/.*\s*=\s*//')"
+      cursor_theme="$(grep 'gtk-cursor-theme-name' "$config" | sed 's/.*\s*=\s*//')"
+      font_name="$(grep 'gtk-font-name' "$config" | sed 's/.*\s*=\s*//')"
+      ${gsettings} set ${gnomeSchema} gtk-theme "$gtk_theme"
+      ${gsettings} set ${gnomeSchema} icon-theme "$icon_theme"
+      ${gsettings} set ${gnomeSchema} cursor-theme "$cursor_theme"
+      ${gsettings} set ${gnomeSchema} font-name "$font_name"
+    '';
+  in {
     enable = true;
     xwayland = true;
     systemdIntegration = true;
