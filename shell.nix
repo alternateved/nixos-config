e9eda19ec9f1bb16c9869f4529ac1e5404f7f0e0
alternateved <alternateved@gmail.com>
2021-10-13 23:26:49 +0200

    Add shell.nix file

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{ pkgs }:
+
+with pkgs;
+let
+  cLibsAndTools =
+    [ pkg-config xorg.libX11 xorg.libXinerama xorg.libXrandr xorg.libXext ];
+
+  haskellTools = [
+    cabal-install
+    ghcid
+    haskellPackages.hindent
+    haskellPackages.hoogle
+    haskellPackages.ormolu
+  ];
+
+in mkShell {
+  name = "flakeEnv";
+  buildInputs = cLibsAndTools ++ haskellTools;
+}
