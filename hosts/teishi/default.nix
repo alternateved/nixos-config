269d5b8779900125280fa773ece8d6fd54e509ac
alternateved <alternateved@gmail.com>
2021-10-31 20:22:56 +0100

    Add IOHK binary caches

--- hosts/teishi/default.nix
+++ hosts/teishi/default.nix
@@ -102,9 +102,15 @@
       dates = [ "weekly" ];
     };
     trustedUsers = [ "alternateved" "root" ];
-    binaryCaches = [ "https://nix-community.cachix.org/" ];
+    binaryCaches = [
+      "https://nix-community.cachix.org/"
+      "https://hydra.iohk.io"
+      "https://iohk.cachix.org"
+    ];
     binaryCachePublicKeys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+      "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
     ];
     package = pkgs.nixUnstable;
     extraOptions = ''
