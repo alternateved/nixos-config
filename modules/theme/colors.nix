58911e5b367f1ae11f59f76020949e655c35f6e2
alternateved <alternateved@gmail.com>
2021-11-25 20:22:18 +0100

    Add kaolin colors

--- modules/theme/colors.nix
+++ modules/theme/colors.nix
@@ -1,71 +1,47 @@
-# Modus operandi
+# Kaolin light
 #{
-#  background = "#ffffff";
-#  foreground = "#000000";
+#  background = "#EDEEEB";
+#  foreground = "#353b3c";
 #
-#  black = "#000000";
-#  red = "#a60000";
-#  green = "#005e00";
-#  yellow = "#813e00";
-#  blue = "#0031a9";
-#  magenta = "#721045";
-#  cyan = "#00538b";
-#  white = "#bfbfbf";
+#  black = "#353b3c";
+#  red = "#e84c58";
+#  green = "#13665F";
+#  yellow = "#E36B3F";
+#  blue = "#3B84CC";
+#  magenta = "#a9779c";
+#  cyan = "#6facb3";
+#  white = "#C8CCC3";
 #
-#  bright-black = "#595959";
-#  bright-red = "#972500";
-#  bright-green = "#315b00";
-#  bright-yellow = "#70480f";
-#  bright-blue = "#2544bb";
-#  bright-magenta = "#5317ac";
-#  bright-cyan = "#005a5f";
-#  bright-white = "#ffffff";
+#  bright-black = "#4b5254";
+#  bright-red = "#e84c58";
+#  bright-green = "#317A56";
+#  bright-yellow = "#C5882C";
+#  bright-blue = "#4C7A90";
+#  bright-magenta = "#6D46E3";
+#  bright-cyan = "#008b8b";
+#  bright-white = "#f5f6f5";
 #}
 
-# Modus vivendi
-{
-  background = "#000000";
-  foreground = "#ffffff";
-
-  black = "#000000";
-  red = "#ff8059";
-  green = "#44bc44";
-  yellow = "#d0bc00";
-  blue = "#2fafff";
-  magenta = "#feacd0";
-  cyan = "#00d3d0";
-  white = "#bfbfbf";
-
-  bright-black = "#595959";
-  bright-red = "#ef8b50";
-  bright-green = "#70b900";
-  bright-yellow = "#c0c530";
-  bright-blue = "#79a8ff";
-  bright-magenta = "#b6a0ff";
-  bright-cyan = "#6ae4b9";
-  bright-white = "#ffffff";
-}
-
 # Kaolin dark
-# {
-#   background = "#18181B";
-#   foreground = "#E4E4E8";
+{
+  background = "#18181B";
+  foreground = "#E4E4E8";
 
-#   black = "#4B5254";
-#   red = "#CD5C60";
-#   green = "#6FB593";
-#   yellow = "#DBAC66";
-#   blue = "#91B9C7";
-#   magenta = "#845A84";
-#   cyan = "#4D9391";
-#   white = "#E4E4E8";
+  black = "#4B5254";
+  red = "#CD5C60";
+  green = "#6FB593";
+  yellow = "#DBAC66";
+  blue = "#91B9C7";
+  magenta = "#845A84";
+  cyan = "#4D9391";
+  white = "#E4E4E8";
 
-#   bright-black = "#879193";
-#   bright-red = "#E36D5B";
-#   bright-green = "#72CCBA";
-#   bright-yellow = "#F2C866";
-#   bright-blue = "#97B8DE";
-#   bright-magenta = "#8C629C";
-#   bright-cyan = "#5096AB";
-#   bright-white = "#EFEFF1";
-# }
+  bright-black = "#879193";
+  bright-red = "#E36D5B";
+  bright-green = "#72CCBA";
+  bright-yellow = "#F2C866";
+  bright-blue = "#97B8DE";
+  bright-magenta = "#8C629C";
+  bright-cyan = "#5096AB";
+  bright-white = "#EFEFF1";
+}
