080fcb5e39aa5bad8aee2a68928571f7c544645c
alternateved <alternateved@gmail.com>
2021-11-30 21:36:15 +0100

    Override old org package

--- modules/emacs/default.nix
+++ modules/emacs/default.nix
@@ -3,82 +3,85 @@
     emacs = {
       enable = true;
       package = pkgs.emacsGcc;
-       extraPackages = (epkgs:
-         with epkgs; [
-           # Package managements
-           use-package
-           auto-compile
+      overrides = self: super: {
+        org = self.elpaPackages.org;
+      };
+      extraPackages = (epkgs:
+        with epkgs; [
+          # Package managements
+          use-package
+          auto-compile
 
-           # Cache
-           gcmh
-           no-littering
+          # Cache
+          gcmh
+          no-littering
 
-           # Editor
-           rainbow-delimiters
-           avy
-           ace-window
-           god-mode
-           multiple-cursors
-           expand-region
+          # Editor
+          rainbow-delimiters
+          avy
+          ace-window
+          god-mode
+          multiple-cursors
+          expand-region
 
-           # Apperance
-           minions
-           kaolin-themes
-           all-the-icons
-           all-the-icons-dired
-           all-the-icons-completion
+          # Apperance
+          minions
+          kaolin-themes
+          all-the-icons
+          all-the-icons-dired
+          all-the-icons-completion
 
-           # Completion framework
-           vertico
-           orderless
-           marginalia
-           consult
-           embark
-           embark-consult
+          # Completion framework
+          vertico
+          orderless
+          marginalia
+          consult
+          embark
+          embark-consult
 
-           # Org
-           org
-           org-bullets
-           org-roam
-           hl-todo
-           olivetti
+          # Org
+          org
+          org-bullets
+          org-roam
+          hl-todo
+          olivetti
 
-           # Tools
-           org-msg
-           elfeed
-           project
-           magit
-           diff-hl
-           eshell-syntax-highlighting
-           eshell-toggle
-           vterm
-           which-key
-           helpful
-           rainbow-mode
+          # Tools
+          org-msg
+          elfeed
+          project
+          magit
+          diff-hl
+          eshell-syntax-highlighting
+          eshell-toggle
+          vterm
+          which-key
+          helpful
+          rainbow-mode
 
-           # Development
-           envrc
-           dumb-jump
-           eglot
-           consult-eglot
-           corfu
-           cape
+          # Development
+          envrc
+          dumb-jump
+          eglot
+          consult-eglot
+          corfu
+          cape
 
-           ## Languages
-           flymake-shellcheck
-           nix-mode
-           js2-mode
-           js2-refactor
-           web-mode
-           typescript-mode
-           json-mode
-           haskell-mode
-           hindent
-           toml-mode
-           yaml-mode
-           lua-mode
-           markdown-mode
-         ]);
+          ## Languages
+          flymake-shellcheck
+          nix-mode
+          js2-mode
+          js2-refactor
+          web-mode
+          typescript-mode
+          json-mode
+          haskell-mode
+          hindent
+          toml-mode
+          yaml-mode
+          lua-mode
+          markdown-mode
+        ]);
     };
   };
   home-manager.users.alternateved.services = { emacs.enable = true; };
