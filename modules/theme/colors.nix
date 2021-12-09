2ca1292cfbb60cb072efef002c8a153df33bd814
alternateved <alternateved@gmail.com>
2021-12-09 21:27:33 +0100

    Add Kaolin galaxy colorscheme

--- modules/theme/colors.nix
+++ modules/theme/colors.nix
@@ -22,26 +22,74 @@
 #  bright-white = "#f5f6f5";
 #}
 
-# Kaolin aurora
+# Kaolin galaxy
 {
-  background = "#14191e";
+  background = "#212026";
   foreground = "#e6e6e8";
 
-  black = "#14191e";
-  red = "#e55c7a";
-  green = "#31E183";
-  yellow = "#f5c791";
-  blue = "#4ca6e8";
-  magenta = "#9d81ba";
-  cyan = "#49bdb0";
+  black = "#212026";
+  red = "#ef6787";
+  green = "#49bdb0";
+  yellow = "#eed891";
+  blue = "#41b0f3";
+  magenta = "#cea2ca";
+  cyan = "#6bd9db";
   white = "#e6e6e8";
 
-  bright-black = "#454459";
-  bright-red = "#cd5c60";
-  bright-green = "#7CF083";
-  bright-yellow = "#dbac66";
-  bright-blue = "#91b9c7";
-  bright-magenta = "#6E6884";
-  bright-cyan = "#0D9C94";
+  bright-black = "#615B75";
+  bright-red = "#D6224D";
+  bright-green = "#6dd797";
+  bright-yellow = "#f5c791";
+  bright-blue = "#2a57cc";
+  bright-magenta = "#9d81ba";
+  bright-cyan = "#0bc9cf";
   bright-white = "#f2f2f2";
 }
+
+# Kaolin dark
+#{
+#  background = "#18181B";
+#  foreground = "#E4E4E8";
+#
+#  black = "#4B5254";
+#  red = "#CD5C60";
+#  green = "#6FB593";
+#  yellow = "#DBAC66";
+#  blue = "#91B9C7";
+#  magenta = "#845A84";
+#  cyan = "#4D9391";
+#  white = "#E4E4E8";
+#
+#  bright-black = "#879193";
+#  bright-red = "#E36D5B";
+#  bright-green = "#72CCBA";
+#  bright-yellow = "#F2C866";
+#  bright-blue = "#97B8DE";
+#  bright-magenta = "#8C629C";
+#  bright-cyan = "#5096AB";
+#  bright-white = "#EFEFF1";
+#}
+
+# Kaolin aurora
+#{
+#  background = "#14191e";
+#  foreground = "#e6e6e8";
+
+#  black = "#14191e";
+#  red = "#e55c7a";
+#  green = "#31E183";
+#  yellow = "#f5c791";
+#  blue = "#4ca6e8";
+#  magenta = "#9d81ba";
+#  cyan = "#49bdb0";
+#  white = "#e6e6e8";
+
+#  bright-black = "#454459";
+#  bright-red = "#cd5c60";
+#  bright-green = "#7CF083";
+#  bright-yellow = "#dbac66";
+#  bright-blue = "#91b9c7";
+#  bright-magenta = "#6E6884";
+#  bright-cyan = "#0D9C94";
+#  bright-white = "#f2f2f2";
+#}
