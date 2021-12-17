865af19003f6b308668720197af4e2363adb9bf9
alternateved <alternateved@gmail.com>
2021-12-17 23:06:39 +0100

    Upgrade teishi

--- hosts/teishi/hardware-configuration.nix
+++ hosts/teishi/hardware-configuration.nix
@@ -1,42 +1,36 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
 
-  boot.initrd.availableKernelModules =
-    [ "i915" "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "i915" "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
-  boot.blacklistedKernelModules = [ "nouveau" "nvidia" ];
+  # boot.blacklistedKernelModules = [ "nouveau" "nvidia" ];
 
-  boot.initrd = {
-    luks.devices."root" = {
-      device =
-        "/dev/disk/by-uuid/5ca9c56c-0372-45e4-8b45-fa8bb5089c3b"; # UUID for /dev/nvme01np2
-      preLVM = true;
-      keyFile = "/keyfile0.bin";
-      allowDiscards = true;
+  fileSystems."/" =
+    { device = "/dev/mapper/nixenc";
+      fsType = "btrfs";
+      options = [ "subvol=nixos" ];
     };
-    secrets = { "keyfile0.bin" = "/etc/secrets/initrd/keyfile0.bin"; };
-  };
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/5f05ca85-3f1d-4c36-b392-bec176def269";
-    fsType = "ext4";
-  };
+  boot.initrd.luks.devices."nixenc".device = "/dev/disk/by-uuid/0b1950c3-8fd5-4402-a757-5fc7bb857070";
 
-  fileSystems."/boot/efi" = {
-    device = "/dev/disk/by-uuid/2DEA-48AF";
-    fsType = "vfat";
-  };
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/383A-EC18";
+      fsType = "vfat";
+    };
 
   swapDevices =
-    [{ device = "/dev/disk/by-uuid/837c185e-1551-4359-9b0b-deb72124aaf7"; }];
+    [ { device = "/dev/disk/by-uuid/3379e138-81de-401c-8ec2-ed925e0063fe"; }
+    ];
 
   powerManagement.cpuFreqGovernor = "performance";
   hardware.cpu.intel.updateMicrocode = true;
-  hardware.nvidiaOptimus.disable = true;
+  # hardware.nvidiaOptimus.disable = true;
   hardware.acpilight.enable = true;
   hardware.opengl.extraPackages = with pkgs; [
     vaapiIntel
