221e254964cd6d6eb5f0f84599c2026a7593d31b
alternateved <alternateved@gmail.com>
2021-12-19 00:39:01 +0100

    Update themes

--- modules/theme/colors.nix
+++ modules/theme/colors.nix
@@ -1,3 +1,51 @@
+# Modus operandi
+{
+  background = "#ffffff";
+  foreground = "#000000";
+
+  black = "#000000";
+  red = "#a60000";
+  green = "#005e00";
+  yellow = "#813e00";
+  blue = "#0031a9";
+  magenta = "#721045";
+  cyan = "#00538b";
+  white = "#bfbfbf";
+
+  bright-black = "#595959";
+  bright-red = "#972500";
+  bright-green = "#315b00";
+  bright-yellow = "#70480f";
+  bright-blue = "#2544bb";
+  bright-magenta = "#5317ac";
+  bright-cyan = "#005a5f";
+  bright-white = "#ffffff";
+}
+
+# Modus vivendi
+#{
+#  background = "#000000";
+#  foreground = "#ffffff";
+#
+#  black = "#000000";
+#  red = "#ff8059";
+#  green = "#44bc44";
+#  yellow = "#d0bc00";
+#  blue = "#2fafff";
+#  magenta = "#feacd0";
+#  cyan = "#00d3d0";
+#  white = "#bfbfbf";
+#
+#  bright-black = "#595959";
+#  bright-red = "#ef8b50";
+#  bright-green = "#70b900";
+#  bright-yellow = "#c0c530";
+#  bright-blue = "#79a8ff";
+#  bright-magenta = "#b6a0ff";
+#  bright-cyan = "#6ae4b9";
+#  bright-white = "#ffffff";
+#}
+
 # Kaolin light
 #{
 #  background = "#EDEEEB";
@@ -22,29 +70,53 @@
 #  bright-white = "#f5f6f5";
 #}
 
+# Kaolin breeze
+#{
+#  background = "#EBE8E4";
+#  foreground = "#383e3f";
+#
+#  black = "#383e3f";
+#  red = "#cd5c60";
+#  green = "#39855f";
+#  yellow = "#b87e3c";
+#  blue = "#2683b5";
+#  magenta = "#845A84";
+#  cyan = "#48a9a9";
+#  white = "#C9C2BD";
+#
+#  bright-black = "#7D8468";
+#  bright-red = "#ef6787";
+#  bright-green = "#3e594e";
+#  bright-yellow = "#d1832e";
+#  bright-blue = "#4F9CB8";
+#  bright-magenta = "#605DB3";
+#  bright-cyan = "#008b8b";
+#  bright-white = "#60696b";
+#}
+
 # Kaolin dark
-{
-  background = "#18181B";
-  foreground = "#E4E4E8";
-
-  black = "#4B5254";
-  red = "#CD5C60";
-  green = "#6FB593";
-  yellow = "#DBAC66";
-  blue = "#91B9C7";
-  magenta = "#845A84";
-  cyan = "#4D9391";
-  white = "#E4E4E8";
-
-  bright-black = "#879193";
-  bright-red = "#E36D5B";
-  bright-green = "#72CCBA";
-  bright-yellow = "#F2C866";
-  bright-blue = "#97B8DE";
-  bright-magenta = "#8C629C";
-  bright-cyan = "#5096AB";
-  bright-white = "#EFEFF1";
-}
+#{
+#  background = "#18181B";
+#  foreground = "#E4E4E8";
+#
+#  black = "#4B5254";
+#  red = "#CD5C60";
+#  green = "#6FB593";
+#  yellow = "#DBAC66";
+#  blue = "#91B9C7";
+#  magenta = "#845A84";
+#  cyan = "#4D9391";
+#  white = "#E4E4E8";
+#
+#  bright-black = "#879193";
+#  bright-red = "#E36D5B";
+#  bright-green = "#72CCBA";
+#  bright-yellow = "#F2C866";
+#  bright-blue = "#97B8DE";
+#  bright-magenta = "#8C629C";
+#  bright-cyan = "#5096AB";
+#  bright-white = "#EFEFF1";
+#}
 
 # Kaolin galaxy
 # {
@@ -92,4 +164,52 @@
 #  bright-magenta = "#6E6884";
 #  bright-cyan = "#0D9C94";
 #  bright-white = "#f2f2f2";
+#}
+
+# Kaolin temple
+#{
+#  background = "#2B2B2F";
+#  foreground = "#EEDCC1";
+
+#  black = "#2B2B2F";
+#  red = "#ef6787";
+#  green = "#47ba99";
+#  yellow = "#eed891";
+#  blue = "#4EB8CA";
+#  magenta = "#fbaed2";
+#  cyan = "#49bdb0";
+#  white = "#EEDCC1";
+
+#  bright-black = "#697375";
+#  bright-red = "#BA667D";
+#  bright-green = "#74B09A";
+#  bright-yellow = "#f5c791";
+#  bright-blue = "#91b9c7";
+#  bright-magenta = "#c791aa";
+#  bright-cyan = "#4FA8A3";
+#  bright-white = "#bebec4";
+#}
+
+# Kaolin ocean
+#{
+#  background = "#14141e";
+#  foreground = "#e6e6e8";
+#
+#  black = "#14141e";
+#  red = "#e84c58";
+#  green = "#35BF88";
+#  yellow = "#dbac66";
+#  blue = "#4ca6e8";
+#  magenta = "#c79af4";
+#  cyan = "#6bd9db";
+#  white = "#e6e6e8";
+#
+#  bright-black = "#545c5e";
+#  bright-red = "#e84c58";
+#  bright-green = "#7CF083";
+#  bright-yellow = "#eed891";
+#  bright-blue = "#53859d";
+#  bright-magenta = "#cea2ca";
+#  bright-cyan = "#4d9391";
+#  bright-white = "#bebec4";
 #}
