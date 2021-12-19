c714ac7917e2d4c70ed3a57fa2fb969cba029c0a
alternateved <alternateved@gmail.com>
2021-12-19 14:24:49 +0100

    Remove dead code

--- overlays/default.nix
+++ overlays/default.nix
@@ -16,14 +16,6 @@ _: pkgs: rec {
           { };
       });
   });
-  bspwm = pkgs.bspwm.overrideAttrs (old: rec {
-    src = pkgs.fetchFromGitHub {
-      owner = "baskerville";
-      repo = "bspwm";
-      rev = "e22d0fad23e0e85b401be69f2360a1c3a0767921";
-      sha256 = "ds+jiDp0hwycfz/YQQy74SOcVmxOqxI9EYoJjp1Vs+E=";
-    };
-  });
   sway = pkgs.sway.overrideAttrs (old: rec {
     src = pkgs.fetchFromGitHub {
       owner = "swaywm";
